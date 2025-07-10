bb99f5cf772106ba48c136a905e17b828dce5cb4
manas_pachauri <manaspachauri31@gmail.com>
2025-07-10 23:20:42 +0530

    new changes made

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-        Copyright 2025 © BlogNow - All Right Reserved.
+        Copyright 2025 © QuickBlog - All Right Reserved.
       </p>
     </div>
   );
